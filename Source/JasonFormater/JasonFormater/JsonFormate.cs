ed4405ca5d1f0d5fbf98e68b9b3f36eb43f16d40
Md Rony <mdrony.pro18@gmail.com>
2022-10-29 19:36:00 +0600

    New Filed Added

---
+++ Source/JasonFormater/JasonFormater/JsonFormate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JasonFormater
+{
+    public static class JsonFormate
+    {
+        public static string Convert<T>(T item)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type type = assembly.GetType(item.ToString());
+            PropertyInfo[] propertyInfos = type.GetProperties(); 
+            string temp = "";
+            foreach (var items in propertyInfos) 
+            {
+                var proval="";
+                if (items.PropertyType == typeof(string) | items.PropertyType == typeof(int) | items.PropertyType == typeof(double) |items.PropertyType==typeof(DateTime))
+                {
+                    proval += "\"";
+                    proval += items.GetValue(item, null);
+                    proval += "\"";
+                    proval += ",";
+                   
+                }
+                temp += "\"" + items.Name + "\"" + ":" + proval;
+                temp += "\n";
+                object val=items.GetValue(item, null);
+                var elements = val as IList;
+                if (elements != null)
+                {
+                    temp += "[";
+                    temp += "\n";
+                    foreach (var pi in elements)
+                    {
+                        
+                        temp += "{";
+                        temp += "\n";
+                        temp += Convert(pi);
+                        temp += "}";
+                        temp += ",";
+                        temp +="\n";
+                        
+                    }
+                    temp += "]";
+                    temp += ",";
+                    temp += "\n";
+                }
+                if (items.PropertyType.Assembly==type.Assembly)
+                {
+                    temp += "{";
+                    temp += "\n";
+                    temp += Convert(items.GetValue(item,null));
+                    temp +="}";
+                    temp += ",";
+                    temp += "\n";
+                }
+            }
+            return temp;
+        }
+
+    }
+}
---
+++ Source/JasonFormater/JasonFormater/Program.cs
@@ -0,0 +1,76 @@
+﻿
+namespace JasonFormater
+{
+    public class MainClass
+    {
+        public static void Main(string[] agrs)
+        {
+            Course course = new Course()
+            {
+                Fees = 30000,
+                Title = "Asp.net",
+                Teacher = new Instructor
+                {
+                    Name = "Jalal Uddin",
+                    Email = "jalal@gmail.com",
+                    PhoneNumbers = new List<Phone>
+                    {
+                        new Phone()
+                        {
+                            Number = "01796136070",
+                            Extension = "GP",
+                            CountryCode = "+880",
+                        },
+                        new Phone()
+                        {
+                            Number = "01796136070",
+                            Extension = "BL",
+                            CountryCode = "+880",
+                        }
+                    },
+                    PermanentAddress = new Address()
+                    {
+                        City = "Bogura",
+                        Street = "SantaharRoad",
+                        Country = "Bangladesh",
+                    },
+                    PresentAddress = new Address()
+                    {
+                        City = "Bogura",
+                        Street = "SantharRoad",
+                        Country = "Bangladesh",
+                    }
+                },
+                Tests = new List<AdmissionTest>
+                {
+                    new AdmissionTest()
+                    {
+                        TestFees=100,
+                        StartDateTime=DateTime.Now,
+                        EndDateTime=DateTime.Now,
+                    }
+                },
+                Topics = new List<Topic>
+                {
+                    new Topic()
+                    {
+                        Description="Description",
+                        Sessions=new List<Session>()
+                        {
+                            new Session()
+                            {
+                                DurationInHour=60,
+                                LearningObjective="LearningObjective",
+                            }
+                        },
+                        Title="Reflection",
+                    }
+                },
+            };
+            string jason = JsonFormate.Convert<Course>(course);
+            Console.WriteLine("{"+"\n" + jason +"}");
+        }
+        
+
+    }
+}
